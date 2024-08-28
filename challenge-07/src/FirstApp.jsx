c1dffa6c1a87ed5fa91f832eec5e2f75eb112de8
Laab1611 <ludy.agudelo@uao.edu.co>
2024-08-28 16:18:16 -0500

    Cositas

---
+++ challenge-07/src/FirstApp.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+const title = 'First App'
+const FirstApp = () => {
+  return (
+    <>
+    <h1>{title}</h1>
+    <span>10</span>
+    </>
+  )
+}
+
+export default FirstApp
